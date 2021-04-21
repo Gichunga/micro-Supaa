323af76f4fb895d811095d88e1604d92de6a473a
steve <gichungasteve6@gmail.com>
2021-04-21 23:16:47 +0000

    configured bootstrap  scafolding

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import "bootstrap";
+import "./assets/app.scss";
+import jQuery from "jquery";
+import "popper.js";
+window.$ = window.jQuery = jQuery;
 
 Vue.config.productionTip = false;
 
