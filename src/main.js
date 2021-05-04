a16a4cf541196d6f8d5f3f05064564ea90611326
steve <gichungasteve6@gmail.com>
2021-05-04 18:43:06 +0000

    configure vuex to use as state management for the app

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import store from "./store";
 import "bootstrap";
 import "./assets/app.scss";
 import $ from "jquery";
@@ -48,6 +49,7 @@ fb.auth().onAuthStateChanged((user) => {
   if (!app) {
     new Vue({
       router,
+      store,
       render: (h) => h(App),
     }).$mount("#app");
   }
